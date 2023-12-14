ba3c6a86deabe8f2fcbcfcdfb4cc19ec49b653aa
Polina Arkashova <arkashova_p@mail.ru>
2023-12-14 22:52:52 +0700

    Added tasks: "Temperature conversion" and "Todo list".

---
+++ TemperatureСonversion/wwwroot/script.js
@@ -0,0 +1,23 @@
+﻿"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const convertButton = document.getElementById("convertButton");
+    const celsiusField = document.getElementById("celsiusField");
+
+    convertButton.addEventListener("click", function (e) {
+        let celsiusTemperature = celsiusField.value.trim();
+
+        celsiusField.classList.remove("invalid");
+
+        if (isNaN(celsiusTemperature)) {
+            celsiusField.classList.add("invalid");
+            return;
+        }
+
+        let fahrenheitTemperature = (9 * celsiusTemperature / 5 + 32).toFixed(2);
+        let kelvinTemperature = (Number(celsiusTemperature) + 273.15).toFixed(2);
+
+        document.getElementById("fahrenheitField").setAttribute("value", fahrenheitTemperature);
+        document.getElementById("kelvinField").setAttribute("value", kelvinTemperature);
+    });
+})
---
+++ ToDoList/wwwroot/script.js
@@ -0,0 +1,72 @@
+﻿"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+    const todoForm = document.getElementById("todo-form");
+    const addNewTaskField = document.getElementById("add-new-task-field");
+    const todolist = document.getElementById("todo-list");
+
+    todoForm.addEventListener("submit", function (e) {
+        e.preventDefault(); /* чтобы форма не отправлялась и не выполнялось действие по умолчанию - перезагрузка страницы */
+
+        let newValue = addNewTaskField.value.trim();
+
+        addNewTaskField.classList.remove("invalid");
+
+        if (newValue.length === 0) {
+            addNewTaskField.classList.add("invalid");
+            return;
+        }
+
+        const listItem = document.createElement("li");
+
+        let listItemValue = newValue;
+
+        setViewMode();
+
+        todolist.append(listItem);
+
+        addNewTaskField.value = "";
+
+        function setViewMode() {
+            listItem.innerHTML = `<span class="list-item-value" >${listItemValue}</span>
+                    <button type="button" class="list-item-edit-button">Редактировать</button>
+                    <button type="button" class="list-item-remove-button">Удалить</button>`;
+
+            listItem.querySelector(".list-item-remove-button").addEventListener("click", function () {
+                listItem.remove();
+            })
+
+            listItem.querySelector(".list-item-edit-button").addEventListener("click", function () {
+                listItem.innerHTML = `
+                    <div>
+                        <input type="text" class="edit-task-field" id="edit-task-field" value="${listItemValue}">
+                        <div class="error-message">Не введен текст</div>
+                    </div>
+                    <button type="button" class="list-item-cancel-button">Отменить</button>
+                    <button type="button" class="list-item-save-button">Сохранить</button>
+                    `;
+
+                listItem.querySelector(".list-item-cancel-button").addEventListener("click", function () {
+                    setViewMode();
+                })
+
+                listItem.querySelector(".list-item-save-button").addEventListener("click", function () {
+
+                    const editTaskField = document.getElementById("edit-task-field");
+
+                    let editedValue = editTaskField.value.trim();
+
+                    editTaskField.classList.remove("invalid");
+
+                    if (editedValue.length === 0) {
+                        editTaskField.classList.add("invalid");
+                        return;
+                    }
+
+                    listItemValue = editedValue;
+
+                    setViewMode();
+                })
+            })
+        }
+    })
+})
