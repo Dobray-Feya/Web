6c96c1e75cd670f02e23d0723dab72f78fa7c5f7
Polina Arkashova <arkashova_p@mail.ru>
2023-12-14 22:46:43 +0700

    Fixed teacher's comments in tasks: "Work with arrays" and "Work with objects".

--- WorkWithArrays/wwwroot/arrays.js
+++ WorkWithArrays/wwwroot/arrays.js
@@ -1,26 +1,10 @@
-﻿// Подзадача 1
+﻿"use strict";
 
 (function () {
-    "use strict"
     console.log("Подзадача 1");
 
-    let array = [8, -9, 3, 3.1, 3.2, -3, 12, 4, 6];
-    printArray(array, "Исходный массив");
-
-    sortDescending(array);
-    printArray(array, "Массив после сортировки по убыванию");
-
-    printArray(getFirstElements(array, 5), "5 первых элементов отсортированного массива");
-    printArray(getLastElements(array, 5), "5 последних элементов отсортированного массива");
-
-    printValue(sumEvenElements(array), "Сумма четных элементов массива");
-
     function sortDescending(array) {
-        if (array === [] || array.length === 1) {
-            return array;
-        }
-
-        array.sort((x, y) => y - x);
+        array.sort((n1, n2) => n2 - n1);
     }
 
     function getFirstElements(array, count) {
@@ -28,24 +12,43 @@
     }
 
     function getLastElements(array, count) {
-        return array.slice(array.length - count);
+        return array.slice(-count);
     }
 
-    function sumEvenElements(array) {
-        return array.filter(x => (x % 2 === 0)).reduce((x, y) => x + y, 0);
+    function getEvenNumbersSum(array) {
+        return array
+            .filter(n => n % 2 === 0)
+            .reduce((n1, n2) => n1 + n2, 0);
     }
-})();
 
-// Подзадача 2
+    function printArray(array, text) {
+        if ((typeof text === "string")) {
+            console.log(text + ":");
+        }
 
-(function () {
-    "use strict"
+        console.log(array.join(", "));
+    }
+
+    function printValue(value, text) {
+        if (typeof text === "string") {
+            console.log(text + ":");
+        }
+
+        console.log(value);
+    }
+
+    const array1 = [8, -9, 3, 3.1, 3.2, -3, 12, 4, 6];
+    printArray(array1, "Исходный массив");
+
+    sortDescending(array1);
+    printArray(array1, "Массив после сортировки по убыванию");
+
+    printArray(getFirstElements(array1, 5), "5 первых элементов отсортированного массива");
+    printArray(getLastElements(array1, 5), "5 последних элементов отсортированного массива");
+
+    printValue(getEvenNumbersSum(array1), "Сумма четных элементов массива");
+
     console.log("Подзадача 2");
-
-    const array = createNumbersArray(100);
-    printArray(array, "Массив чисел от 1 до 100");
-
-    printArray(getEvenElementsSquares(array), "Квадраты четных элементов массива");
 
     function createNumbersArray(count) {
         let array = [];
@@ -57,26 +60,14 @@
         return array;
     }
 
-    function getEvenElementsSquares(array) {
-        return array.filter(x => (x % 2 === 0)).map(x => x * x);
+    function getEvenNumbersSquares(array) {
+        return array
+            .filter(x => (x % 2 === 0))
+            .map(x => x * x);
     }
 
-})()
+    const array2 = createNumbersArray(100);
+    printArray(array2, "Массив чисел от 1 до 100");
 
-// Глобальные функция - общие для обеих подзадач
-
-function printArray(array, text) {
-    if ((typeof text === "string")) {
-        console.log(text + ":");
-    }
-
-    console.log(array.join(", "));
-}
-
-function printValue(value, text) {
-    if ((typeof text === "string")) {
-        console.log(text + ":");
-    }
-
-    console.log(value);
-}
+    printArray(getEvenNumbersSquares(array2), "Квадраты четных элементов массива");
+})();
--- WorkWithObjects/wwwroot/objects.js
+++ WorkWithObjects/wwwroot/objects.js
@@ -1,71 +1,80 @@
 ﻿(function () {
-    const russia = {
-        "name": "Россия",
-        cities: [
-            { "name": "Екатеринбург", "population": "1493600" },
-            { "name": "Казань", "population": "1259173" },
-            { "name": "Москва", "population": "13015126" },
-            { "name": "Нижний Новгород", "population": "1233949" },
-            { "name": "Новосибирск", "population": "1621330" },
-            { "name": "Санкт-Петербург", "population": "5607916" },
-            { "name": "Челябинск", "population": "1179288" },
-        ]
-    }
-
-    const china = {
-        "name": "Китай",
-        cities: [
-            { "name": "Гуанчжоу", "population": "14043500" },
-            { "name": "Пекин", "population": "21705000" },
-            { "name": "Тяньцзинь", "population": "15469500" },
-            { "name": "Ухань", "population": "10607700" },
-            { "name": "Чунцин", "population": "30751600" },
-            { "name": "Шанхай", "population": "24152700" },
-            { "name": "Шеньчжэнь", "population": "11908400" },
-        ]
-    }
-
-    const india = {
-        "name": "Индия",
-        cities: [
-            { "name": "Ахмадабад", "population": "5570585" },
-            { "name": "Бангалор", "population": "8425970" },
-            { "name": "Дели", "population": "11007835" },
-            { "name": "Мумбаи", "population": "12478447" },
-            { "name": "Хайдарабад", "population": "6809970" },
-            { "name": "Ченнаи", "population": "4681087" },
-        ]
-    }
-
-    const countries = [russia, china, india];
-
-    let countriesWithMaxCitiesNumber = [];
-    let maxCitiesNumber = 0;
-
-    countries.forEach((country) => {
-        let citiesNumber = country.cities.length;
-
-        if (citiesNumber > maxCitiesNumber) {
-            maxCitiesNumber = citiesNumber;
-            countriesWithMaxCitiesNumber = [country];
+    const countries = [
+        {
+            name: "Россия",
+            cities: [
+                { name: "Екатеринбург", "population": "1493600" },
+                { name: "Казань", "population": "1259173" },
+                { name: "Москва", "population": "13015126" },
+                { name: "Нижний Новгород", "population": "1233949" },
+                { name: "Новосибирск", "population": "1621330" },
+                { name: "Санкт-Петербург", "population": "5607916" },
+                { name: "Челябинск", "population": "1179288" }
+            ]
+        },
+        {
+            name: "Китай",
+            cities: [
+                { name: "Гуанчжоу", "population": "14043500" },
+                { name: "Пекин", "population": "21705000" },
+                { name: "Тяньцзинь", "population": "15469500" },
+                { name: "Ухань", "population": "10607700" },
+                { name: "Чунцин", "population": "30751600" },
+                { name: "Шанхай", "population": "24152700" },
+                { name: "Шеньчжэнь", "population": "11908400" }
+            ]
+        },
+        {
+            name: "Индия",
+            cities: [
+                { name: "Ахмадабад", "population": "5570585" },
+                { name: "Бангалор", "population": "8425970" },
+                { name: "Дели", "population": "11007835" },
+                { name: "Мумбаи", "population": "12478447" },
+                { name: "Хайдарабад", "population": "6809970" },
+                { name: "Ченнаи", "population": "4681087" }
+            ]
         }
-        else if (citiesNumber == maxCitiesNumber) {
-                countriesWithMaxCitiesNumber.push(country);
-        }
-    });
+    ];
 
-    console.log(`Страны с наибольшим числом городов (${maxCitiesNumber}):`);
-    console.log(countriesWithMaxCitiesNumber.map(country => country.name).join(", "));
+    function getCountriesNamesWithMaxCitiesCount(countries) {
+        let countriesWithmaxCitiesCount = [];
+        let maxCitiesCount = 0;
 
-    let countryPopulations = {};
+        countries.forEach((country) => {
+            const citiesCount = country.cities.length;
 
-    countries.forEach((country) => {
-        countryPopulations[country.name] = country.cities.map(city => city.population).reduce((x, y) => { return Number(x) + Number(y) }, 0) / 1000000;
-    });
+            if (citiesCount > maxCitiesCount) {
+                maxCitiesCount = citiesCount;
+                countriesWithmaxCitiesCount = [country];
+            } else if (citiesCount === maxCitiesCount) {
+                countriesWithmaxCitiesCount.push(country);
+            }
+        });
 
-    console.log(`Численность населения по странам (млн человек):`);
-
-    for (let countryName in countryPopulations) {
-        console.log(`${countryName} - ${countryPopulations[countryName]}`);
+        return countriesWithmaxCitiesCount.map(country => country.name);
     }
-})()
+
+    console.log(`Страны с наибольшим числом городов:`);
+    console.log(getCountriesNamesWithMaxCitiesCount(countries).join(", "));
+
+    function getContriesPopulations(countries) {
+        let countriesPopulations = {};
+
+        countries.forEach(country => {
+            countriesPopulations[country.name] = country.cities
+                .map(city => city.population)
+                .reduce((e1, e2) => Number(e1) + Number(e2), 0)
+        });
+
+        return countriesPopulations;
+    }
+
+    console.log(`Численность населения по странам:`);
+
+    const countriesPopulations = getContriesPopulations(countries);
+
+    for (let countryName in countriesPopulations) {
+        console.log(`${countryName} - ${new Intl.NumberFormat("ru-RU").format(countriesPopulations[countryName])}`);
+    }
+})();
