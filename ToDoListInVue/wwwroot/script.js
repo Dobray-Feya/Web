489bd92a4ddf5a1b63130976f28d7443c63d736e
Polina Arkashova <arkashova_p@mail.ru>
2024-01-06 09:37:21 +0700

    Vue task. First commit.

--- ToDoListInVue/wwwroot/script.js
+++ ToDoListInVue/wwwroot/script.js
@@ -1,19 +1,67 @@
-﻿"use strict";
-
-(function () {
-    Vue.createApp({
-        // data() - краткий синтаксис из ES 6, аналогично data: function() { … }
+﻿Vue.createApp({})
+    .component("TodoList", {
         data() {
             return {
-                message: "hello!"
-            };
+                items: [],
+                newTodoItemId: 1,
+                newTodoItemText: ""
+            }
         },
 
         methods: {
-            changeMessage() {
-                this.message = this.message.indexOf("Привет") >= 0
-                    ? "Пока, Vue!" : "Привет, Vue!";
+            addNewTodoItem() {
+                const newTodoItem = {
+                    id: this.newTodoItemId,
+                    text: this.newTodoItemText
+                };
+
+                this.items.push(newTodoItem);
+
+                this.newTodoItemId++;
+                this.newTodoItemText = "";
             }
-        }
-    }).mount("#form");
-})();
+        },
+
+        template: `
+        <form @submit.prevent="addNewTodoItem" class="row mb-3">
+            <label class="col">
+                <input type="text" v-model="newTodoItemText" class="form-control"/>
+            </label>
+            <div class="col-auto">
+                <button class="btn btn-primary">Добавить</button>
+            </div>
+        </form>
+        <ul class="list-unstyled">
+            <todo-list-item v-for="item in items"
+                            :key="item.id" 
+                            :item="item">
+            </todo-list-item>
+        </ul>`
+    })
+    .component("TodoListItem", {
+        props: {
+            item: {
+                type: Object,
+                required: true
+            }
+        },
+
+        data() {
+            return {
+                isEditing: false
+            }
+        },
+
+        template: `
+        <li>
+            <div v-if="!isEditing" class="row">
+                <span class="col me-1">{{ item.text }}</span>
+                <button type="button" class="btn btn-primary col-auto me-1">Редактировать</button>
+                <button type="button" class="btn btn-danger col-auto">Удалить</button>
+           </div>
+           <div v-else>
+               <input type="text" v-model="item.text">
+           </div>
+        </li>`
+    })
+.mount("#app")
