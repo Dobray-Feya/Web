3c0e748dd9862bdbd055e51d364a74b0f852a28e
Polina Arkashova <arkashova_p@mail.ru>
2024-02-18 11:58:25 +0700

    Phone book WebPack task. Fixed teacher's comments.

--- PhoneBookWebPack/frontend/js/App.vue
+++ PhoneBookWebPack/frontend/js/App.vue
@@ -1,30 +1,54 @@
 ﻿<template>
     <div class="container">
-        <h1 class="mb-3 mt-3">Контакты</h1>
 
+        <h1 class="mb-3 mt-3">Контакты</h1>
         <form @submit.prevent="createContact">
             <h2 class="h5 mt-3">Добавить контакт</h2>
-
             <div class="row">
-                <div class="col-4">
-                    <input v-model.trim="name" type="text" name="name" class="form-control" placeholder="Имя">
+                <div class="col-4 mb-2 mx-2 px-0">
+                    <input v-model.trim="name"
+                           type="text"
+                           class="form-control"
+                           placeholder="Имя"
+                           required>
                 </div>
-                <div class="col-4">
-                    <input v-model.trim="phone" type="text" name="phone" class="form-control ms-2" placeholder="Телефон">
+                <div class="col-3 mb-2 mx-2 px-0">
+                    <input v-model.trim="phone"
+                           type="text"
+                           class="form-control"
+                           placeholder="Телефон"
+                           required>
                 </div>
-                <button type="submit" class="btn btn-primary col-auto ms-2">Добавить</button>
-                <button @click="clear" type="button" class="btn btn-secondary col-auto ms-2">Очистить</button>
+                <button type="submit"
+                        class="btn btn-primary col-auto mb-2 mx-2">
+                    Добавить
+                </button>
+                <button type="button"
+                        @click="clearContacts"
+                        class="btn btn-secondary col-auto mb-2 mx-2">
+                    Очистить
+                </button>
             </div>
         </form>
 
         <form @submit.prevent="loadContacts">
-            <h2 class="h5 mt-5">Найти контакты</h2>
-
+            <h2 class="h5 mt-3">Найти контакты</h2>
             <div class="row">
-                <div class="col-4">
-                    <input v-model.trim="term" type="text" name="term" class="form-control" placeholder="Поиск">
+                <div class="col-4 mb-2 mx-2 px-0">
+                    <input v-model.trim="term"
+                           type="text"
+                           class="form-control"
+                           placeholder="Поиск">
                 </div>
-                <button type="submit" class="btn btn-primary col-auto ms-2">Поиск</button>
+                <button type="submit"
+                        class="btn btn-primary col-auto mb-2 mx-2">
+                    Поиск
+                </button>
+                <button type="button"
+                        @click="clearTerm"
+                        class="btn btn-secondary col-auto mb-2 mx-2">
+                    Очистить
+                </button>
             </div>
         </form>
 
@@ -53,14 +77,26 @@
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <h1 class="modal-title fs-5" id="modalLabel">Подтвердите удаление</h1>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            <button type="button"
+                                                    class="btn-close"
+                                                    data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
                                         </div>
                                         <div class="modal-body">
                                             Контакт (имя: {{ contact.name }}, телефон: {{ contact.phone }}) будет удален из телефонной книги.
                                         </div>
                                         <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Отменить</button>
-                                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="deleteContact(contact)">Удалить</button>
+                                            <button type="button"
+                                                    class="btn btn-secondary"
+                                                    data-bs-dismiss="modal">
+                                                Отменить
+                                            </button>
+                                            <button type="button"
+                                                    class="btn btn-primary"
+                                                    data-bs-dismiss="modal"
+                                                    @click="deleteContact(contact)">
+                                                Удалить
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
@@ -69,21 +105,57 @@
                     </tr>
                     <tr v-else>
                         <td>{{ index + 1 }}</td>
-                        <td><input v-model.trim="contact.newName"></td>
-                        <td><input v-model.trim="contact.newPhone"></td>
                         <td>
-                            <i class="bi bi-check-circle button me-3" @click="saveContact(contact)" title="Сохранить"></i>
-                            <i class="bi bi-dash-circle button" @click="cancelEditing(contact)" title="Отменить"></i>
+                            <input type="text"
+                                   v-model.trim="contact.newName"
+                                   class="form-control">
+                        </td>
+                        <td>
+                            <input type="text"
+                                   v-model.trim="contact.newPhone"
+                                   class="form-control">
+                        </td>
+                        <td>
+                            <i class="bi bi-check-circle button me-3"
+                               @click="saveContact(contact)"
+                               title="Сохранить">
+                            </i>
+                            <i class="bi bi-dash-circle button"
+                               @click="cancelEditing(contact)"
+                               title="Отменить">
+                            </i>
                         </td>
                     </tr>
                 </tbody>
             </table>
         </div>
+
+        <!-- Диалог для показа сообщений об ошибке -->
+        <div class="modal fade" id="errorModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="modalLabel">Произошла ошибка</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        {{ errorText }}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button"
+                                class="btn btn-secondary"
+                                data-bs-dismiss="modal">
+                            Понятно
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+    import bootstrap from "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 
     import PhoneBookService from "./phoneBookService";
 
@@ -93,10 +165,10 @@
         data() {
             return {
                 contacts: [],
-                id: 1,      // id, name, phone - параметры нового контакта
-                name: "",
+                name: "",      // name, phone - параметры нового контакта
                 phone: "",
-                term: "",   // строка поиска
+                term: "",      // строка поиска
+                errorText: "", // текcт ошибки от сервера, если во время выполнения запроса произошла ошибка
                 service: new PhoneBookService()
             };
         },
@@ -109,42 +181,49 @@
             loadContacts() {
                 this.service.getContacts(this.term).then(contacts => {
                     this.contacts = contacts;
-                }).catch(() => alert("Не удалось загрузить контакты"));
+                }).catch(() => this.showError("Не удалось загрузить контакты"));
+            },
+
+            showError(text) {
+                this.errorText = text;
+                const errorModal = new bootstrap.Modal("#errorModal");
+                errorModal.show();
             },
 
             createContact() {
                 const contact = {
-                    id: this.id,  //в модель передаются только id, name, phone
                     name: this.name,
-                    phone: this.phone,
-                    isEditing: false, //isEditing, newName, newPhone используются только во VM
-                    newName: "",
-                    newPhone: ""
+                    phone: this.phone
                 }
 
+                // В модель передаем только name и phone
                 this.service.createContact(contact).then(response => {
                     if (!response.success) {
-                        alert(response.message);
+                        this.showError(response.message);
                         return;
                     }
 
-                    this.id++;
+                    // Добавляем дополнительные поля, которые нужны только во VM
+                    contact.isEditing = false;
+                    contact.newName = "";
+                    contact.newPhone = "";
+
                     this.name = "";
                     this.phone = "";
 
                     this.loadContacts();
-                }).catch(() => alert("Не удалось создать контакт"));
+                }).catch(() => this.showError("Не удалось создать контакт"));
             },
 
             deleteContact(contact) {
                 this.service.deleteContact(contact.id).then(response => {
                     if (!response.success) {
-                        alert(response.message);
+                        showError(response.message);
                         return;
                     }
 
                     this.loadContacts();
-                }).catch(() => alert("Не удалось удалить контакт"));
+                }).catch(() => this.showError("Не удалось удалить контакт"));
             },
 
             editContact(contact) {
@@ -171,22 +250,23 @@
 
                 this.service.updateContact(updatedContact).then(response => {
                     if (!response.success) {
-                        alert(response.message);
+                        this.showError(response.message);
                         return;
                     }
 
-                    contact.name = newName;
-                    contact.phone = newPhone;
-                    contact.newName = "";
-                    contact.newPhone = "";
-                    contact.isEditing = false;
-                }).catch(() => alert("Не удалось обновить контакт"));
+                    this.loadContacts();
+                }).catch(() => this.showError("Не удалось обновить контакт"));
             },
 
-            clear() {
+            clearContacts() {
                 this.name = "";
                 this.phone = "";
+            },
+
+            clearTerm() {
+                this.term = "";
+                this.loadContacts();
             }
         }
-};
+    };
 </script>
--- PhoneBookWebPack/routes/index.js
+++ PhoneBookWebPack/routes/index.js
@@ -1,18 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+let contacts = []; // { name, phone }
+let currentId = 1;
 
 /* GET home page. */
 router.get("/", function (req, res) {
     res.render("index", { title: "PhoneBook Vue Server" });
 });
 
-let contacts = []; // { name, phone }
-let newContact = {}; // del?
-let isEditing = false;
-
-
 /* api/getContacts?term=search-text */
-router.get('/api/getContacts', function (req, res) {
+router.get("/api/getContacts", function (req, res) {
     const term = (req.query.term || "").toUpperCase();
 
     const result = term.length === 0
@@ -23,21 +21,30 @@ router.get('/api/getContacts', function (req, res) {
 });
 
 /* id:4 */
-router.delete('/api/deleteContact/:id', function (req, res) {
-    const id = req.params.id;
+router.delete("/api/deleteContact/:id", function (req, res) {
+    const id = Number(req.params.id);
 
-    contacts = contacts.filter(c => c.id != id);  // ?? !==
+    const contactCountBeforeDeleting = contacts.length;
 
-    res.send({
-        success: true,
-        message: `Контакт удален (id: ${id})`
-    });
+    contacts = contacts.filter(c => c.id !== id);
+
+    if (contactCountBeforeDeleting === contacts.length) {
+        res.send({
+            success: false,
+            message: `Контакт с id ${id} не найден`
+        });
+    } else {
+        res.send({
+            success: true,
+            message: `Контакт удален (id: ${id})`
+        });
+    }
 });
 
-// { id, name, phone }
-router.post('/api/createContact', function (req, res) {
+// { name, phone }
+router.post("/api/createContact", function (req, res) {
     const contact = {
-        id: req.body.id,
+        id: currentId,
         name: req.body.name,
         phone: req.body.phone
     };
@@ -65,20 +72,22 @@ router.post('/api/createContact', function (req, res) {
     if (contacts.some(c => c.phone.toUpperCase() === upperCasePhone)) {
         res.send({
             success: false,
-            message: "Контакт с таким номером телефона уже существует"
+            message: `Контакт с номером телефона ${upperCasePhone} уже существует`
         });
 
         return;
     }
 
     contacts.push(contact);
+    currentId++;
 
     res.send({
         success: true,
         message: `Контакт создан (id: ${contact.id}, name: ${contact.name}, phone: ${contact.phone})`
     });
 });
-router.post('/api/updateContact', function (req, res) {
+
+router.post("/api/updateContact", function (req, res) {
     const id = req.body.id;
     const newName = req.body.name;
     const newPhone = req.body.phone;
@@ -103,21 +112,28 @@ router.post('/api/updateContact', function (req, res) {
 
     const newPhoneInUpperCase = newPhone.toUpperCase();
 
-    if (contacts.some(c => (c.id !== id && c.phone.toUpperCase() === newPhoneInUpperCase))) {
+    if (contacts.some(c => c.id !== id && c.phone.toUpperCase() === newPhoneInUpperCase)) {
         res.send({
             success: false,
-            message: `Контакт с номером ${newPhone} уже существует`
+            message: `Контакт с номером телефона ${newPhone} уже существует`
         });
 
         return;
     }
 
-    contacts.forEach(c => {
-        if (c.id === id) {
-            c.name = newName;
-            c.phone = newPhone;
-        }
-    });
+    const updatedContact = contacts.find(c => c.id === id);
+
+    if (!updatedContact) {
+        res.send({
+            success: false,
+            message: `Контакта с id ${id} не существует`
+        });
+
+        return;
+    }
+
+    updatedContact.name = newName;
+    updatedContact.phone = newPhone;
 
     res.send({
         success: true,
